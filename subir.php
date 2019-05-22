68107d4c2e410a28f77328cbcd6e56a5deba9bc4
James Atachagua Flores <James@MacBook-Pro-de-James-2.local>
2019-05-22 14:14:45 -0500

    first commit

--- subir.php
+++ subir.php
@@ -5,14 +5,14 @@
 	$tamano_imagen = $_FILES['imagen']['size'];
 	$temp = $_FILES['imagen']['tmp_name'];
 
-	//echo $_FILES['imagen']['size'];
+	echo $_FILES['imagen']['size'];
 
 
 	$carpeta_destino = '/var/www/html/imagenes_bd/';
 
-move_uploaded_file($temp, $carpeta_destino.$nombre_imagen);
+	move_uploaded_file($temp, $carpeta_destino.$nombre_imagen);
 
-		echo "hecho";
+		//echo "hecho";
 	
 
 ?>
