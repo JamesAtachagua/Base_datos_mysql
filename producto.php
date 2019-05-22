a8fc57573a9b54816e3aeeea312a2f97569df10e
James Atachagua Flores <James@MacBook-Pro-de-James-2.local>
2019-05-22 08:22:10 -0500

    first commit

--- producto.php
+++ producto.php
@@ -80,7 +80,7 @@ if(isset($_REQUEST['action']))
                         <tr>
                             <th style="text-align:left;">Imagen</th>
                             <td>
-                                <input type="file" name="imagen" value="<?php echo $alm->addslashes(file_get_contents(_FILES['imagen']['temp_name'])); ?>" size="20" />
+                                <input type="file" name="imagen" value="<?php echo $alm->__FILES['imagen']['temp_name']; ?>" size="20" />
                             </td>
                         </tr>
                         
