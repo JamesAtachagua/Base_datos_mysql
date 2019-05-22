b9adf5ff090a231c8b9601f8d7da28f84f46a64a
James Atachagua Flores <James@MacBook-Pro-de-James-2.local>
2019-05-22 10:51:24 -0500

    first commit

--- producto.php
+++ producto.php
@@ -80,7 +80,7 @@ if(isset($_REQUEST['action']))
                         </tr>
                         <tr>
 					     	<td style="text-align:left;">Imagen</td>
-					        <td><input class="input-group" type="file" name="image" accept="image/*" value="<?php echo $alm->$_FILES['imagen']['name'];; ?>" /></td>
+					        <td><input type="file" name="image" accept="image/*" value="<?php echo $_FILES['imagen']['name']; ?>" /></td>
 					    </tr>
                     
                         <tr>
